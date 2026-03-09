f2b1e27057a063c9e9a3ced733f4b7bda35b3442
shakaylac <kaycnite@gmail.com>
2026-03-09 12:08:16 -0500

    I created functions to show and hide the description div as well as event listeners for mouseenter and mouseleave so that the div appears on enter and disappears on leave. Foe some reason it only works on the first image and only if the mouse goes over a specific part of the image container.

--- JS/gallery-veiwer.js
+++ JS/gallery-veiwer.js
@@ -1,13 +1,15 @@
 const imageContainer = document.getElementById('gallery_image');
+const imageDescription = document.getElementById('image_description');
 
 
+function showImageDescription() {
+    imageDescription.style.display = "block";
+}
 
-imageContainer.addEventListener("mouseenter", function() {
-    const imageDescription = document.createElement('div');
-    imageDescription.style.backgroundColor = "#000";
-    imageDescription.style.filter.brightness = "80%";
-    imageDescription.style.width = "40%";
-    imageDescription.style.height = "auto";
-    console.log("Mouse entered image");
-});
+function hideImageDescription() {
+    imageDescription.style.display = "none";
+}
    
+
+imageContainer.addEventListener("mouseenter", showImageDescription);
+imageContainer.addEventListener("mouseleave", hideImageDescription);
